a8661052c37e3229ced971647586823d720a751b
cheoryeon <rlacjfgus2@gmail.com>
2020-06-11 22:47:38 +0900

    Chage editText size
    chage editText size to wrap_content

--- app/src/main/java/com/example/snoker/SmokeInfoActivity.kt
+++ app/src/main/java/com/example/snoker/SmokeInfoActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SmokeInfoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_smoke_info)
