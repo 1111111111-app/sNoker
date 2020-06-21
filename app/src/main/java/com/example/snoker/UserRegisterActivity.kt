8d2e63821c33a3839db10e208efc156feda9292d
cheoryeon <rlacjfgus2@gmail.com>
2020-06-21 17:55:21 +0900

    Update Intro Activity
    update activity view
    add font xml

--- app/src/main/java/com/example/snoker/UserRegisterActivity.kt
+++ app/src/main/java/com/example/snoker/UserRegisterActivity.kt
@@ -1,12 +1,10 @@
 package com.example.snoker
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_intro.*
 import kotlinx.android.synthetic.main.activity_user_register.*
 
 class UserRegisterActivity : AppCompatActivity() {
